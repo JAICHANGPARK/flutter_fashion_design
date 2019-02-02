b6157a9bd59340a3212cd4ed1608d7bd841e9d12
Dreamwalker <aristojeff@gmail.com>
2019-02-02 21:06:38 +0900

    Update main.dart

--- flutter_fashion_design/lib/main.dart
+++ flutter_fashion_design/lib/main.dart
@@ -7,10 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: MyHome(),
     );
   }
@@ -26,13 +24,76 @@ class _MyHomeState extends State<MyHome> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Discovery",
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 12.0
-        ),),
-
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: Text(
+          'Discovery',
+          style: TextStyle(
+              fontSize: 22.0,
+              color: Colors.black,
+              fontFamily: 'Montserrat',
+              fontWeight: FontWeight.bold),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.camera),
+            color: Colors.grey,
+            onPressed: () {},
+          )
+        ],
       ),
+      body: ListView(
+        padding: EdgeInsets.only(
+          top: 15.0,
+          bottom: 15.0,
+        ),
+        children: <Widget>[
+          Container(
+            height: 150.0,
+            width: double.infinity,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.all(10.0),
+              children: <Widget>[],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget listItem(String imgPath, String logo) {
+    return Column(
+      children: <Widget>[
+        Stack(
+          children: <Widget>[
+
+            //사람 사진 담을 것
+            Container(
+              height: 75.0,
+              width: 75.0,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(37.5),
+                  image: DecorationImage(
+                      image: AssetImage(imgPath), fit: BoxFit.cover)),
+            ),
+
+            // 디자인회사 로고
+            Positioned(
+              top: 50.0,
+              left: 50.0,
+              child: Container(
+                height: 25.0,
+                width: 25.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.5),
+                    image: DecorationImage(
+                        image: AssetImage(logo), fit: BoxFit.cover)),
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 }
